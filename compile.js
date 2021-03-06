2ab8cb79b19d912b730801e342b5e5fc6ada4e7f
HotSpicyRamen <xiovuvoix@gmail.com>
2021-03-06 11:53:56 +0300

    Structural changes was made

--- compile.js
+++ compile.js
@@ -1,5 +1,21 @@
-const sass = require("sass");
+const sass = require("node-sass");
+const path = require("path");
+const fs = require("fs");
 
-sass.render({ file: "./sass/style.scss" }, (error, result) => {
-  if (error) throw error;
-});
+sass.render(
+  {
+    file: "./scss/root.scss",
+    sourceMap: true,
+    sourceComments: false,
+    outFile: "./dist/serpent.css",
+    outputStyle: "compressed",
+    precision: 10,
+  },
+  (error, result) => {
+    if (error) throw error;
+
+    fs.writeFile("./dist/serpent.css", result.css, (error) => {
+      if (error) throw error;
+    });
+  }
+);
